7f13e3285823c1254d363b139ec12ac4d28263fe
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-01-31 18:54:41 +0300

    Update linter errors in Js file

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-let nav = document.querySelector("#nav-list");
-let navToggle = document.querySelector("#nav-toggle");
-let closeNav = document.querySelector("#nav-close");
-let navLink = document.querySelectorAll(".nav-link");
+const nav = document.querySelector('#nav-list');
+const navToggle = document.querySelector('#nav-toggle');
+const closeNav = document.querySelector('#nav-close');
+const navLink = document.querySelectorAll('.nav-link');
 
-function show(){
-    nav.classList.toggle("active");
+function show() {
+  nav.classList.toggle('active');
 }
 
-function hide(){
-    nav.classList.toggle("active");
+function hide() {
+  nav.classList.toggle('active');
 }
 
-function removeClass(){
-    nav.classList.remove("active");
+function removeClass() {
+  nav.classList.remove('active');
 }
 
 navToggle.addEventListener('click', show);
 closeNav.addEventListener('click', hide);
 
-for (let i = 0; i <= navLink.length; i++){
-    navLink[i].addEventListener("click", removeClass);
+for (let i = 0; i <= navLink.length; i += 1) {
+  navLink[i].addEventListener('click', removeClass);
 }
