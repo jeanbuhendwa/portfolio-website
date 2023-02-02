e7927811f8512b269d9174876d4040fe8293335e
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-02-02 14:46:42 +0300

    Add the work section array in popup js file

--- popUp.js
+++ popUp.js
@@ -1,13 +1,106 @@
+const workSection = document.getElementById('work-section-container');
+const section = document.createElement('section');
+workSection.appendChild(section);
+
+section.innerHTML += `<section class="work-section" id="portfolio">
+<h2>My Recent Works</h2>
+<div id="hr-line"></div>
+<div class="project-container">
+  <div class="work">
+    <div class="project-info">
+      <h3>Multi-Post Stories Gain+Glory</h3>
+      <ul>
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavScript</li>
+        <li>html</li>
+      </ul>
+      <button onclick="projectDetails(0)" name="See Project">See Project</button>
+    </div>
+  </div>
+  <div class="work">
+    <div class="project-info">
+      <h3>Multi-Post Stories Gain+Glory</h3>
+      <ul>
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavScript</li>
+        <li>html</li>
+      </ul>
+      <button onclick="projectDetails(1)" name="See Project">See Project</button>
+    </div>
+  </div>
+  <div class="work">
+    <div class="project-info">
+      <h3>Multi-Post Stories Gain+Glory</h3>
+      <ul>
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavScript</li>
+        <li>html</li>
+      </ul>
+      <button onclick="projectDetails(2)" name="See Project">See Project</button>
+    </div>
+  </div>
+  <div class="work">
+    <div class="project-info">
+      <h3>Multi-Post Stories Gain+Glory</h3>
+      <ul>
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavScript</li>
+        <li>html</li>
+      </ul>
+      <button onclick="projectDetails(3)" name="See Project">See Project</button>
+    </div>
+  </div>
+  <div class="work">
+    <div class="project-info">
+      <h3>Multi-Post Stories Gain+Glory</h3>
+      <ul>
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavScript</li>
+        <li>html</li>
+      </ul>
+      <button onclick="projectDetails(4)" name="See Project">See Project</button>
+    </div>
+  </div>
+  <div class="work">
+    <div class="project-info">
+      <h3>Multi-Post Stories Gain+Glory</h3>
+      <ul>
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavScript</li>
+        <li>html</li>
+      </ul>
+      <button onclick="projectDetails(5)" name="See Project">See Project</button>
+    </div>
+  </div>
+</div>
+</section>
+
+`;
+
 const projects = [
   {
     projectId: 1,
     projectName: 'Keeping track of hundreds of components',
-    projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    projectDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: 'img/projects/projectone-img.png',
     gitIcon: 'img/icons/git-icon.svg',
     liveIcon: 'img/icons/liveIcon.svg',
     techMobile: ['Ruby on rails', 'css', 'JavScript'],
-    techDesktop: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
+    techDesktop: [
+      'Codekit',
+      'GitHub',
+      'Javasript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
     sourceLink: 'https://github.com/jeanbuhendwa/portfolio-website',
     liveLink: 'https://jeanbuhendwa.github.io/portfolio-website/',
   },
@@ -15,12 +108,20 @@ const projects = [
   {
     projectId: 2,
     projectName: 'Keeping track of hundreds of components',
-    projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    projectDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: 'img/projects/projectone-img.png',
     gitIcon: 'img/icons/git-icon.svg',
     liveIcon: 'img/icons/liveIcon.svg',
     techMobile: ['Ruby on rails', 'css', 'JavScript'],
-    techDesktop: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
+    techDesktop: [
+      'Codekit',
+      'GitHub',
+      'Javasript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
     sourceLink: 'https://github.com/jeanbuhendwa/portfolio-website',
     liveLink: 'https://jeanbuhendwa.github.io/portfolio-website/',
   },
@@ -28,12 +129,20 @@ const projects = [
   {
     projectId: 3,
     projectName: 'Keeping track of hundreds of components',
-    projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    projectDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: 'img/projects/projectone-img.png',
     gitIcon: 'img/icons/git-icon.svg',
     liveIcon: 'img/icons/liveIcon.svg',
     techMobile: ['Ruby on rails', 'css', 'JavScript'],
-    techDesktop: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
+    techDesktop: [
+      'Codekit',
+      'GitHub',
+      'Javasript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
     sourceLink: 'https://github.com/jeanbuhendwa/portfolio-website',
     liveLink: 'https://jeanbuhendwa.github.io/portfolio-website/',
   },
@@ -41,12 +150,20 @@ const projects = [
   {
     projectId: 4,
     projectName: 'Keeping track of hundreds of components',
-    projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    projectDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: 'img/projects/projectone-img.png',
     gitIcon: 'img/icons/git-icon.svg',
     liveIcon: 'img/icons/liveIcon.svg',
     techMobile: ['Ruby on rails', 'css', 'JavScript'],
-    techDesktop: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
+    techDesktop: [
+      'Codekit',
+      'GitHub',
+      'Javasript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
     sourceLink: 'https://github.com/jeanbuhendwa/portfolio-website',
     liveLink: 'https://jeanbuhendwa.github.io/portfolio-website/',
   },
@@ -54,12 +171,20 @@ const projects = [
   {
     projectId: 5,
     projectName: 'Keeping track of hundreds of components',
-    projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    projectDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: 'img/projects/projectone-img.png',
     gitIcon: 'img/icons/git-icon.svg',
     liveIcon: 'img/icons/liveIcon.svg',
     techMobile: ['Ruby on rails', 'css', 'JavScript'],
-    techDesktop: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
+    techDesktop: [
+      'Codekit',
+      'GitHub',
+      'Javasript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
     sourceLink: 'https://github.com/jeanbuhendwa/portfolio-website',
     liveLink: 'https://jeanbuhendwa.github.io/portfolio-website/',
   },
@@ -67,12 +192,20 @@ const projects = [
   {
     projectId: 6,
     projectName: 'Keeping track of hundreds of components',
-    projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    projectDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     imageDesktop: 'img/projects/projectone-img.png',
     gitIcon: 'img/icons/git-icon.svg',
     liveIcon: 'img/icons/liveIcon.svg',
     techMobile: ['Ruby on rails', 'css', 'JavScript'],
-    techDesktop: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
+    techDesktop: [
+      'Codekit',
+      'GitHub',
+      'Javasript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
     sourceLink: 'https://github.com/jeanbuhendwa/portfolio-website',
     liveLink: 'https://jeanbuhendwa.github.io/portfolio-website/',
   },
