1d97ab422fc87998cfad7eb3093be6af9963b4ba
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-06 23:20:58 +0300

    Fix linters

--- scripts/scrol.js
+++ scripts/scrol.js
@@ -1,16 +1,16 @@
-const scrollTopButton = document.getElementById("scroll-to-top");
+const scrollTopButton = document.getElementById('scroll-to-top');
 
 window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollTopButton.style.display = "block";
+    scrollTopButton.style.display = 'block';
   } else {
-    scrollTopButton.style.display = "none";
+    scrollTopButton.style.display = 'none';
   }
 };
 
 scrollTopButton.onclick = function () {
   window.scrollTo({
     top: 0,
-    behavior: "smooth",
+    behavior: 'smooth',
   });
 };
